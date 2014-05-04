d05d6cf606183dbc2f176a751fdbfca64e6da3b2
Josh Bush <josh@digitalbush.com>
2014-05-04 15:54:51 -0500

    0.2

--- src/Autofac.Settings/Properties/AssemblyInfo.cs
+++ src/Autofac.Settings/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Josh Bush")]
 [assembly: AssemblyProduct("Autofac.Settings")]
-[assembly: AssemblyCopyright("Copyright © Josh Bush 2011-2012")]
+[assembly: AssemblyCopyright("Copyright © Josh Bush 2011-2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
